474a4198d3ff90b129cc7a7a035d0359230638f7
Matías Insaurralde <matias@insaurral.de>
2025-06-23 03:59:46 -0300

    Separate DHT package
    Signed-off-by: Matías Insaurralde <matias@insaurral.de>

--- cmd/dht/dht.go
+++ cmd/dht/dht.go
@@ -10,45 +10,12 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/ipfs/go-cid"
-	"github.com/libp2p/go-libp2p"
-	dht "github.com/libp2p/go-libp2p-kad-dht"
-	"github.com/libp2p/go-libp2p/core/crypto"
-	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/network"
-	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/multiformats/go-multiaddr"
 	"go.uber.org/zap"
 
-	"github.com/matiasinsaurralde/crowdllama/internal/discovery"
 	"github.com/matiasinsaurralde/crowdllama/internal/keys"
 	"github.com/matiasinsaurralde/crowdllama/pkg/config"
-	"github.com/matiasinsaurralde/crowdllama/pkg/crowdllama"
+	"github.com/matiasinsaurralde/crowdllama/pkg/dht"
 )
-
-// defaultListenAddrs is the default listen addresses for the DHT:
-var defaultListenAddrs = []string{
-	"/ip4/0.0.0.0/tcp/9000",
-	"/ip4/0.0.0.0/udp/9000/quic-v1",
-}
-
-// getWorkerNamespaceCID generates the same namespace CID as the worker
-func getWorkerNamespaceCID() cid.Cid {
-	namespaceCID, err := discovery.GetWorkerNamespaceCID()
-	if err != nil {
-		panic("Failed to get namespace CID: " + err.Error())
-	}
-	return namespaceCID
-}
-
-// requestWorkerMetadata requests metadata from a worker peer
-func requestWorkerMetadata(ctx context.Context, h host.Host, workerPeer peer.ID, logger *zap.Logger) (*crowdllama.Resource, error) {
-	metadata, err := discovery.RequestWorkerMetadata(ctx, h, workerPeer, logger)
-	if err != nil {
-		return nil, fmt.Errorf("request worker metadata: %w", err)
-	}
-	return metadata, nil
-}
 
 func main() {
 	if err := runDHTServer(); err != nil {
@@ -108,143 +75,43 @@ func runDHTServer() error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	h, kadDHT, err := newDHTServer(ctx, privKey, logger)
+	// Create DHT server using the new package
+	server, err := dht.NewDHTServer(ctx, privKey, logger)
 	if err != nil {
-		return fmt.Errorf("failed to start DHT: %w", err)
-	}
-	printHostInfo(h, logger)
-
-	// Set up network notifier to detect new connections
-	h.Network().Notify(&network.NotifyBundle{
-		ConnectedF: func(_ network.Network, conn network.Conn) {
-			peerID := conn.RemotePeer().String()
-			logger.Info("New peer connected",
-				zap.String("peer_id", peerID),
-				zap.String("remote_addr", conn.RemoteMultiaddr().String()),
-				zap.String("direction", conn.Stat().Direction.String()))
-		},
-		DisconnectedF: func(_ network.Network, conn network.Conn) {
-			logger.Info("Peer disconnected",
-				zap.String("peer_id", conn.RemotePeer().String()),
-				zap.String("remote_addr", conn.RemoteMultiaddr().String()))
-		},
-	})
-
-	// Start periodic worker discovery
-	go discoverWorkersPeriodically(kadDHT, logger)
-
-	logger.Info("Bootstrapping DHT network")
-	if err := kadDHT.Bootstrap(ctx); err != nil {
-		return fmt.Errorf("failed to bootstrap DHT: %w", err)
+		return fmt.Errorf("failed to create DHT server: %w", err)
 	}
 
+	// Print host information
+	printHostInfo(server, logger)
+
+	// Start the DHT server
+	primaryAddr, err := server.Start()
+	if err != nil {
+		return fmt.Errorf("failed to start DHT server: %w", err)
+	}
+
+	logger.Info("DHT server started successfully", zap.String("primary_addr", primaryAddr))
 	logger.Info("DHT server running. Press Ctrl+C to exit.")
-	waitForShutdown(logger)
+
+	waitForShutdown(logger, server)
 	return nil
 }
 
-func newDHTServer(ctx context.Context, privKey crypto.PrivKey, logger *zap.Logger) (host.Host, *dht.IpfsDHT, error) {
-	logger.Debug("Creating libp2p host with identity",
-		zap.Strings("listen_addrs", defaultListenAddrs))
-
-	h, err := libp2p.New(
-		libp2p.ListenAddrStrings(defaultListenAddrs...),
-		libp2p.Identity(privKey),
-	)
-	if err != nil {
-		logger.Error("Failed to create libp2p host", zap.Error(err))
-		return nil, nil, fmt.Errorf("create libp2p host: %w", err)
-	}
-
-	logger.Debug("Creating DHT instance", zap.String("mode", "server"))
-	kadDHT, err := dht.New(ctx, h, dht.Mode(dht.ModeServer))
-	if err != nil {
-		logger.Error("Failed to create DHT instance", zap.Error(err))
-		return nil, nil, fmt.Errorf("create DHT instance: %w", err)
-	}
-
-	logger.Info("DHT server created successfully",
-		zap.String("peer_id", h.ID().String()),
-		zap.String("dht_mode", "server"))
-
-	return h, kadDHT, nil
-}
-
-func printHostInfo(h host.Host, logger *zap.Logger) {
+func printHostInfo(server *dht.DHTServer, logger *zap.Logger) {
 	logger.Info("DHT Server information",
-		zap.String("peer_id", h.ID().String()))
+		zap.String("peer_id", server.GetPeerID()))
 
 	logger.Debug("DHT Server multiaddresses:")
-	for _, addr := range h.Addrs() {
-		fullAddr := fmt.Sprintf("%s/p2p/%s", addr.String(), h.ID().String())
-		logger.Debug("  Listening address", zap.String("address", fullAddr))
+	for _, addr := range server.GetPeerAddrs() {
+		logger.Debug("  Listening address", zap.String("address", addr))
 	}
 }
 
-func waitForShutdown(logger *zap.Logger) {
+func waitForShutdown(logger *zap.Logger, server *dht.DHTServer) {
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
 	<-sigCh
 	logger.Info("Shutting down DHT server...")
+	server.Stop()
 	time.Sleep(1 * time.Second)
-}
-
-func discoverWorkersPeriodically(kadDHT *dht.IpfsDHT, logger *zap.Logger) {
-	ticker := time.NewTicker(10 * time.Second) // Run every 10 seconds for testing
-	defer ticker.Stop()
-
-	namespaceCID := getWorkerNamespaceCID()
-	logger.Info("Starting periodic worker discovery",
-		zap.String("namespace_cid", namespaceCID.String()),
-		zap.Duration("interval", 10*time.Second))
-
-	for range ticker.C {
-		logger.Debug("Searching for workers advertising namespace",
-			zap.String("namespace_cid", namespaceCID.String()))
-
-		providers := kadDHT.FindProvidersAsync(context.Background(), namespaceCID, 10)
-		workerCount := 0
-
-		for provider := range providers {
-			workerCount++
-			logger.Info("Found worker",
-				zap.String("worker_peer_id", provider.ID.String()),
-				zap.Strings("addresses", multiaddrsToStrings(provider.Addrs)))
-
-			// Request metadata from the worker
-			metadata, err := requestWorkerMetadata(context.Background(), kadDHT.Host(), provider.ID, logger)
-			if err != nil {
-				logger.Error("Failed to get metadata from worker",
-					zap.String("worker_peer_id", provider.ID.String()),
-					zap.Error(err))
-			} else {
-				logger.Info("Worker metadata retrieved successfully",
-					zap.String("worker_peer_id", provider.ID.String()),
-					zap.String("gpu_model", metadata.GPUModel),
-					zap.Int("vram_gb", metadata.VRAMGB),
-					zap.Float64("tokens_throughput", metadata.TokensThroughput),
-					zap.Float64("current_load", metadata.Load),
-					zap.Strings("supported_models", metadata.SupportedModels),
-					zap.Time("last_updated", metadata.LastUpdated))
-			}
-		}
-
-		if workerCount == 0 {
-			logger.Debug("No workers found advertising namespace",
-				zap.String("namespace_cid", namespaceCID.String()))
-		} else {
-			logger.Info("Worker discovery completed",
-				zap.String("namespace_cid", namespaceCID.String()),
-				zap.Int("total_workers_found", workerCount))
-		}
-	}
-}
-
-// multiaddrsToStrings converts multiaddrs to string slice for logging
-func multiaddrsToStrings(addrs []multiaddr.Multiaddr) []string {
-	result := make([]string, len(addrs))
-	for i, addr := range addrs {
-		result[i] = addr.String()
-	}
-	return result
 }
---
+++ pkg/dht/dht.go
@@ -0,0 +1,231 @@
+// Package dht provides DHT server functionality for CrowdLlama.
+package dht
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ipfs/go-cid"
+	"github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/libp2p/go-libp2p/core/crypto"
+	"github.com/libp2p/go-libp2p/core/host"
+	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/multiformats/go-multiaddr"
+	"go.uber.org/zap"
+
+	"github.com/matiasinsaurralde/crowdllama/internal/discovery"
+	"github.com/matiasinsaurralde/crowdllama/pkg/crowdllama"
+)
+
+// DefaultListenAddrs is the default listen addresses for the DHT server
+var DefaultListenAddrs = []string{
+	"/ip4/0.0.0.0/tcp/9000",
+	"/ip4/0.0.0.0/udp/9000/quic-v1",
+}
+
+// DHTServer represents a DHT server instance
+type DHTServer struct {
+	Host      host.Host
+	DHT       *dht.IpfsDHT
+	logger    *zap.Logger
+	ctx       context.Context
+	cancel    context.CancelFunc
+	peerAddrs []string
+}
+
+// NewDHTServer creates a new DHT server instance
+func NewDHTServer(ctx context.Context, privKey crypto.PrivKey, logger *zap.Logger) (*DHTServer, error) {
+	return NewDHTServerWithAddrs(ctx, privKey, logger, DefaultListenAddrs)
+}
+
+// NewDHTServerWithAddrs creates a new DHT server instance with custom listen addresses
+func NewDHTServerWithAddrs(ctx context.Context, privKey crypto.PrivKey, logger *zap.Logger, listenAddrs []string) (*DHTServer, error) {
+	logger.Debug("Creating libp2p host with identity",
+		zap.Strings("listen_addrs", listenAddrs))
+
+	h, err := libp2p.New(
+		libp2p.ListenAddrStrings(listenAddrs...),
+		libp2p.Identity(privKey),
+	)
+	if err != nil {
+		logger.Error("Failed to create libp2p host", zap.Error(err))
+		return nil, fmt.Errorf("create libp2p host: %w", err)
+	}
+
+	logger.Debug("Creating DHT instance", zap.String("mode", "server"))
+	kadDHT, err := dht.New(ctx, h, dht.Mode(dht.ModeServer))
+	if err != nil {
+		logger.Error("Failed to create DHT instance", zap.Error(err))
+		return nil, fmt.Errorf("create DHT instance: %w", err)
+	}
+
+	// Generate peer addresses in the required format
+	peerAddrs := make([]string, 0, len(h.Addrs()))
+	for _, addr := range h.Addrs() {
+		fullAddr := fmt.Sprintf("%s/p2p/%s", addr.String(), h.ID().String())
+		peerAddrs = append(peerAddrs, fullAddr)
+	}
+
+	serverCtx, cancel := context.WithCancel(ctx)
+
+	server := &DHTServer{
+		Host:      h,
+		DHT:       kadDHT,
+		logger:    logger,
+		ctx:       serverCtx,
+		cancel:    cancel,
+		peerAddrs: peerAddrs,
+	}
+
+	logger.Info("DHT server created successfully",
+		zap.String("peer_id", h.ID().String()),
+		zap.String("dht_mode", "server"),
+		zap.Strings("peer_addrs", peerAddrs))
+
+	return server, nil
+}
+
+// Start starts the DHT server and returns the primary peer address
+func (s *DHTServer) Start() (string, error) {
+	// Set up network notifier to detect new connections
+	s.Host.Network().Notify(&network.NotifyBundle{
+		ConnectedF: func(_ network.Network, conn network.Conn) {
+			peerID := conn.RemotePeer().String()
+			s.logger.Info("New peer connected",
+				zap.String("peer_id", peerID),
+				zap.String("remote_addr", conn.RemoteMultiaddr().String()),
+				zap.String("direction", conn.Stat().Direction.String()))
+		},
+		DisconnectedF: func(_ network.Network, conn network.Conn) {
+			s.logger.Info("Peer disconnected",
+				zap.String("peer_id", conn.RemotePeer().String()),
+				zap.String("remote_addr", conn.RemoteMultiaddr().String()))
+		},
+	})
+
+	// Start periodic worker discovery
+	go s.discoverWorkersPeriodically()
+
+	s.logger.Info("Bootstrapping DHT network")
+	if err := s.DHT.Bootstrap(s.ctx); err != nil {
+		return "", fmt.Errorf("failed to bootstrap DHT: %w", err)
+	}
+
+	// Return the first peer address (usually the most accessible one)
+	if len(s.peerAddrs) > 0 {
+		return s.peerAddrs[0], nil
+	}
+	return "", fmt.Errorf("no peer addresses available")
+}
+
+// Stop stops the DHT server
+func (s *DHTServer) Stop() {
+	s.logger.Info("Stopping DHT server...")
+	s.cancel()
+	s.Host.Close()
+}
+
+// GetPeerID returns the DHT server's peer ID
+func (s *DHTServer) GetPeerID() string {
+	return s.Host.ID().String()
+}
+
+// GetPeerAddrs returns all peer addresses in the required format
+func (s *DHTServer) GetPeerAddrs() []string {
+	return s.peerAddrs
+}
+
+// GetPrimaryPeerAddr returns the primary peer address (first in the list)
+func (s *DHTServer) GetPrimaryPeerAddr() string {
+	if len(s.peerAddrs) > 0 {
+		return s.peerAddrs[0]
+	}
+	return ""
+}
+
+// discoverWorkersPeriodically periodically discovers workers advertising the namespace
+func (s *DHTServer) discoverWorkersPeriodically() {
+	ticker := time.NewTicker(10 * time.Second) // Run every 10 seconds for testing
+	defer ticker.Stop()
+
+	namespaceCID := s.getWorkerNamespaceCID()
+	s.logger.Info("Starting periodic worker discovery",
+		zap.String("namespace_cid", namespaceCID.String()),
+		zap.Duration("interval", 10*time.Second))
+
+	for {
+		select {
+		case <-ticker.C:
+			s.logger.Debug("Searching for workers advertising namespace",
+				zap.String("namespace_cid", namespaceCID.String()))
+
+			providers := s.DHT.FindProvidersAsync(context.Background(), namespaceCID, 10)
+			workerCount := 0
+
+			for provider := range providers {
+				workerCount++
+				s.logger.Info("Found worker",
+					zap.String("worker_peer_id", provider.ID.String()),
+					zap.Strings("addresses", s.multiaddrsToStrings(provider.Addrs)))
+
+				// Request metadata from the worker
+				metadata, err := s.requestWorkerMetadata(context.Background(), provider.ID)
+				if err != nil {
+					s.logger.Error("Failed to get metadata from worker",
+						zap.String("worker_peer_id", provider.ID.String()),
+						zap.Error(err))
+				} else {
+					s.logger.Info("Worker metadata retrieved successfully",
+						zap.String("worker_peer_id", provider.ID.String()),
+						zap.String("gpu_model", metadata.GPUModel),
+						zap.Int("vram_gb", metadata.VRAMGB),
+						zap.Float64("tokens_throughput", metadata.TokensThroughput),
+						zap.Float64("current_load", metadata.Load),
+						zap.Strings("supported_models", metadata.SupportedModels),
+						zap.Time("last_updated", metadata.LastUpdated))
+				}
+			}
+
+			if workerCount == 0 {
+				s.logger.Debug("No workers found advertising namespace",
+					zap.String("namespace_cid", namespaceCID.String()))
+			} else {
+				s.logger.Info("Worker discovery completed",
+					zap.String("namespace_cid", namespaceCID.String()),
+					zap.Int("total_workers_found", workerCount))
+			}
+		case <-s.ctx.Done():
+			return
+		}
+	}
+}
+
+// getWorkerNamespaceCID generates the same namespace CID as the worker
+func (s *DHTServer) getWorkerNamespaceCID() cid.Cid {
+	namespaceCID, err := discovery.GetWorkerNamespaceCID()
+	if err != nil {
+		panic("Failed to get namespace CID: " + err.Error())
+	}
+	return namespaceCID
+}
+
+// requestWorkerMetadata requests metadata from a worker peer
+func (s *DHTServer) requestWorkerMetadata(ctx context.Context, workerPeer peer.ID) (*crowdllama.Resource, error) {
+	metadata, err := discovery.RequestWorkerMetadata(ctx, s.Host, workerPeer, s.logger)
+	if err != nil {
+		return nil, fmt.Errorf("request worker metadata: %w", err)
+	}
+	return metadata, nil
+}
+
+// multiaddrsToStrings converts multiaddrs to string slice for logging
+func (s *DHTServer) multiaddrsToStrings(addrs []multiaddr.Multiaddr) []string {
+	result := make([]string, len(addrs))
+	for i, addr := range addrs {
+		result[i] = addr.String()
+	}
+	return result
+}
